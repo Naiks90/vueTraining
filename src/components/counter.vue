e5781387809b0fb7c417699bdb35a8f61cbb8fd6
Naiks90 <sasha.naiko@gmail.com>
2022-01-14 12:03:34 +0200

    props added

---
+++ src/components/counter.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="counter" :class="{ aqua: counter > 2 }">
+    <h2>Счётчик {{ counter }}</h2>
+    <button @click="add()">add</button>
+    <button @click="counter -= 1">sub</button>
+    <button @click="counter = 0">res</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ['counter'],
+  // data() {
+  //   return {
+  //     counter: 0,
+  //   };
+  // },
+  methods: {
+    add() {
+      this.counter += 1;
+    },
+  },
+};
+</script>
+<style>
+.counter {
+  border: 2px solid black;
+  margin-bottom: 25px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+}
+/* .counter:hover {
+  background-color: cadetblue;
+} */
+.aqua {
+  background-color: aqua;
+}
+</style>
